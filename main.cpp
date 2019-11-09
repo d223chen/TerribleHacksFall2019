6990108d1b06b7a139b1fc3422b6111359a10ea7
Daniel Chen <d223chen@csclub.uwaterloo.ca>
2019-11-09 14:21:30 -0500

    Performance optimization

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <deque>
 #include <unistd.h>
 #include <time.h>
+#include <vector>
 
 enum Atom{
 	Hydrogen,
@@ -18,9 +19,9 @@ enum Atom{
 
 std::map<Atom, int> barrier; //tracks how many atoms are at the barrier.
 
-std::deque<char> tunnel; //what atoms are in the tunnel.
+std::map<char, std::deque<int>> tunnel; //what atoms are in the tunnel.
 std::mutex m; //protect the tunnel
-int maxLength = 100; //length of the tunnel...
+int pipeLength = 100; //length of the tunnel...
 
 void keyboardMonitor(){
 	char c = '0';
@@ -36,19 +37,21 @@ void keyboardMonitor(){
 			continue;
 		}
 		m.lock();
-		tunnel.push_front(c);	
+		tunnel[c].push_front(2); 	
 		m.unlock();
 	}
 }
 
+/*
 void pushMonitor(){ //move the tunnel forward
 	while(1){
 		m.lock();
 		tunnel.push_front(' ');
 		m.unlock();
-		usleep(1000 * 500);
+		usleep(1000 * 100);
 	}
 }
+*/
 
 //void animateScreen(
 
@@ -63,16 +66,27 @@ int main(){
 	barrier[Hydrogen] = 0;
 	barrier[Oxygen] = 0;
 	std::thread keym(keyboardMonitor);
-	std::thread pm(pushMonitor);	
+	//std::thread pm(pushMonitor);	
 	while(1){ //console screen
 		waddch(stdscr, '>'); //ejector...
-		for(char ch : tunnel){
-			waddch(stdscr, ch);
+		for(std::pair<const char, std::deque<int>>& p : tunnel){
+			for(int i = 0; i < p.second.size(); i++){
+				int x = p.second[i];
+				int ch = p.first;
+				wmove(stdscr, 0, x - 1); //clear previous position!
+				waddch(stdscr, ' ');
+				wmove(stdscr, 0, x);
+				waddch(stdscr, ch);
+				p.second[i] = p.second[i] + 1;
+				if(p.second[i] > pipeLength){
+					p.second.pop_back();
+				}	
+			}
 			
 		}
 		wmove(stdscr, 0, 1);
 		
-		usleep(1000 * 500);
+		usleep(1000 * 100);
 	}	
 
 	endwin(); //end curses screen
